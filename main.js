f9d527e147e74e1cf1ea900631435d694d1b8558
Adair Juneo <adair_juneo@hotmail.com>
2021-11-11 00:20:17 -0300

    Projeto em funcionamento. Novas features serão em breve.

--- main.js
+++ main.js
@@ -23,48 +23,60 @@ for(const close of cancelar){
 const telaTransacao = {
     open(){
         document.querySelector('.tela-lancamento').classList.add('active')
-        document.body.style.overflow = "hidden"
+        document.body.style.overflowY = "hidden"
     },
     close(){
         document.querySelector('.tela-lancamento').classList.remove('active')
+        document.body.style.overflowY = "visible"
     }
 }
 
-const listaTransacoes = [{
-    id: 001,
-    descricao: 'Desenvolvimento de site',
-    valores: 1200000,
-    data: '13/03/2020'
-},{
-    id: 002,
-    descricao: 'Hamburguer',
-    valores: -5900,
-    data: '10/04/2021'
-},{
-    id: 003,
-    descricao: 'Aluguel de apartamento',
-    valores: -120000,
-    data: '27/03/2020'
-},{
-    id: 004,
-    descricao: 'Computador',
-    valores: -540000,
-    data: '15/03/2020'
-},{
-    id: 005,
-    descricao: 'App de vendas',
-    valores: 700000,
-    data: '26/10/2021'
+const bancoDados = {
+    get(){
+        return JSON.parse(localStorage.getItem("financas:transacoes")) || []
+    },
+
+    set(transacoes) {
+        localStorage.setItem("financas:transacoes", JSON.stringify(transacoes))
+    }
 }
-]
 
 const transacoes = {
-    all: listaTransacoes,
+    all: bancoDados.get(),
     
+    /*[{
+        descricao: 'Desenvolvimento de site',
+        valores: 1200000,
+        data: '13/03/2020'
+    },{
+        descricao: 'Hamburguer',
+        valores: -5900,
+        data: '10/04/2021'
+    },{
+        descricao: 'Aluguel de apartamento',
+        valores: -120000,
+        data: '27/03/2020'
+    },{
+        descricao: 'Computador',
+        valores: -540000,
+        data: '15/03/2020'
+    },{
+        descricao: 'App de vendas',
+        valores: 700000,
+        data: '26/10/2021'
+    }
+    ]*/
+
     add(listaTransacoes){
         transacoes.all.push(listaTransacoes)
 
-        console.log(transacoes.all)
+        app.reload()
+    },
+
+    remove(index){
+        transacoes.all.splice(index, 1)
+
+        app.reload()
     },
 
     entradas(){
@@ -100,7 +112,20 @@ const transacoes = {
     }
 }
 
+//CONSTANTE DE FORMATAÇÕES, LOCAL ONDE FUNÇÕES DE FORMATAÇÃO SÃO CRIADAS E UTILIZADAS NO CÓDIGO
 const formatacoes = {
+    formatarValor(valores){
+        valores = Number(valores) * 100
+
+        return valores
+    },
+
+    formatarData(data){
+        const dataSeparada = data.split("-")
+
+        return `${dataSeparada[2]}/${dataSeparada[1]}/${dataSeparada[0]}`
+    },
+
     formatarMoeda(valores){
         const sinalPositivoNegativo = Number(valores) < 0 ? "-" : ""
 
@@ -121,13 +146,15 @@ const formatacoes = {
 //CONSTANTE PRINCIPAL RESP. POR TODA MODELAGEM DAS TRANSACOES QUE SERÁ INSERIDAS OU REMOVIDAS PELO USUÁRIO
 const modelagemHTML = {
     containerTransacoes: document.querySelector('#data-table tbody'),
+
     addTransacao(listaTransacoes, index){
         let transacao = document.createElement('tr')
-        transacao.innerHTML = modelagemHTML.inserirHtmlTransacao(listaTransacoes)
+        transacao.innerHTML = modelagemHTML.inserirHtmlTransacao(listaTransacoes, index)
+        transacao.dataset.index = index
 
         modelagemHTML.containerTransacoes.appendChild(transacao)
     },
-    inserirHtmlTransacao(listaTransacoes){
+    inserirHtmlTransacao(listaTransacoes, index){
         // VAR RESP. POR VERIFICAR O SINAL DO VALOR(POSITIVO OU NEGATIVO) APLICANDO UMA CLASSE AO ESQUELETO
         let classeNegativoPositivo = listaTransacoes.valores > 0 ? "positivo" : "negativo"
 
@@ -140,7 +167,7 @@ const modelagemHTML = {
         <td class="valores ${classeNegativoPositivo}">${valor}</td>
         <td class="data">${listaTransacoes.data}</td>
         <td class="remover">
-        <button title="Remover Transação" class="btn-remover">
+        <button onclick="transacoes.remove(${index})" title="Remover Transação" class="btn-remover">
             <img
                 src="src/remove.svg"
                 alt="Icone para remover transação"
@@ -148,6 +175,8 @@ const modelagemHTML = {
         </button>
         </td>
         `
+        console.log(index)
+        console.log(listaTransacoes)
         return html
     },
 
@@ -157,23 +186,95 @@ const modelagemHTML = {
         document.getElementById('saidas').innerHTML = formatacoes.formatarMoeda(transacoes.saidas())
 
         document.getElementById('total').innerHTML = formatacoes.formatarMoeda(transacoes.balanco())
+    },
+
+    limparTransacoes() {
+        modelagemHTML.containerTransacoes.innerHTML = ""
     }
 }
 
-const App = {
-    
+//CONSTANTE DE CAPTURA DOS DADOS DO FORMULARIO DE TRANSACOES
+const formulario = {
+    descricao: document.querySelector('input#description'),
+    valores: document.querySelector('input#amount'),
+    data: document.querySelector('input#date'),
+
+    guardarValores(){
+        return{
+            descricao: formulario.descricao.value,
+            valores: formulario.valores.value,
+            data: formulario.data.value
+        }
+    },
+
+    validarCampos() {
+        const { descricao, valores, data } = formulario.guardarValores() 
+
+        if(descricao.trim() === "" || valores.trim() === "" || data.trim() === ""){
+            throw new Error("Por favor, preencha todos os campos.")
+        }
+    },
+
+    tratamentoDados() {
+        let { descricao, valores, data } = formulario.guardarValores()
+
+        valores = formatacoes.formatarValor(valores)
+
+        data = formatacoes.formatarData(data)
+
+        return {
+            descricao, valores, data
+        }
+    },
+
+    limparCampos() {
+        formulario.descricao.value = ""
+        formulario.valores.value = ""
+        formulario.data.value = ""
+    },
+
+    salvarTransacoes(listaTransacoes) {
+        transacoes.add(listaTransacoes)
+    },
+
+    submit(event) {
+        event.preventDefault()
+
+        try{
+            //VALIDANDO OS CAMPOS
+            formulario.validarCampos()
+            //TRANTANDO OS DADOS
+            const dadosDaTransacao = formulario.tratamentoDados()
+            //SALVANDO OS DADOS NA TABELA
+            formulario.salvarTransacoes(dadosDaTransacao)
+            //LIMPANDO OS CAMPOS DO FORMULARIO
+            formulario.limparCampos()
+            //FECHANDO A TELA DE TRANSACOES
+            telaTransacao.close()
+
+        } catch (error) {
+            alert(error.message)
+        }
+    }
 }
 
-// ESTRUTURA FOR-EACH REPONSAVEL POR INSERIR OS AS TRANSACOES NA TABELA DE ACORDO A QUANTIDADE NA LISTA
-listaTransacoes.forEach((listaTransacoes) => {
-    modelagemHTML.addTransacao(listaTransacoes)
-});
+//CONSTANTE QUE DA INÍCIO E RECORRENCIA AS FUNCIONALIDADES
+const app = {
+    init(){
+        // ESTRUTURA FOR-EACH REPONSAVEL POR INSERIR OS AS TRANSACOES NA TABELA DE ACORDO A QUANTIDADE NA LISTA
+        transacoes.all.forEach((listaTransacoes, index) => {
+            modelagemHTML.addTransacao(listaTransacoes, index)
+        })        
 
-modelagemHTML.atualizarBalanco()
+        modelagemHTML.atualizarBalanco()
 
-transacoes.add({
-    id: 10,
-    descricao: 'Almoço',
-    valores: 29,
-    date: '20/10/2021'
-})
+        bancoDados.set(transacoes.all)
+    },
+
+    reload() {
+        modelagemHTML.limparTransacoes()
+        app.init()
+    }
+}
+
+app.init()
